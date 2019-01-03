07f082cc188435f8686d84cfef56eb58e6b57e90
mmaslank <michal.maslanka.7@gmail.com>
2019-01-03 18:36:14 +0100

    MERGING TEMPLATES

--- views/footer.php
+++
@@ -1,8 +0,0 @@
-<footer>
-    <hr>
-    <div class="row">
-        <div class="col-md-1"></div>
-        <div class="col-md-10">This template was developer by Benjamin Castillo Arriaga.  Contact: benjamin.castillo.arriaga@gmail.com </div>
-        <div class="col-md-1"></div>            
-    </div>    
-</footer>
