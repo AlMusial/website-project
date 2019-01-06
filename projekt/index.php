49b0d7a70798f8a2354c0a3464d633722be6c803
mmaslank <michal.maslanka.7@gmail.com>
2019-01-06 22:07:30 +0100

    ruter

--- projekt/index.php
+++ projekt/index.php
@@ -1,6 +1,7 @@
 <?php
-include 'router.php';
+include 'Router.php';
 $request = $_SERVER['REQUEST_URI'];
 $router = new Router($request);
 $router->get('/', 'views/default');
+$router->get('users', 'views/users');
 ?>
